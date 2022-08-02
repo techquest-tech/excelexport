45f07161b60988675709d91f35cf2c13b65f15c0
armenpn <armenpn@gmail.com>
2022-08-02 15:07:30 +0800

    init project.

---
+++ excelexport.go
@@ -0,0 +1,165 @@
+package excelexport
+
+import (
+	"fmt"
+
+	"github.com/xuri/excelize/v2"
+	"go.uber.org/zap"
+)
+
+type Sheet struct {
+	File    *excelize.File
+	StyleID int
+	Name    string
+	Row     int
+	Col     int
+}
+
+func (s *Sheet) GetAxis() string {
+	if s.Col == 0 {
+		s.Col = 1
+	}
+	strCol, err := excelize.ColumnNumberToName(s.Col)
+	if err != nil {
+		panic(err)
+	}
+	axis := fmt.Sprintf("%s%d", strCol, s.Row)
+	return axis
+}
+
+func (s *Sheet) SetHeader(header *ExcelHeader) {
+	if s.Col == 0 {
+		s.Col = 1
+	}
+	strCol, err := excelize.ColumnNumberToName(s.Col)
+	if err != nil {
+		panic(err)
+	}
+	axis := fmt.Sprintf("%s%d", strCol, s.Row)
+
+	if header.Width > 0.0 {
+		s.File.SetColWidth(s.Name, strCol, strCol, header.Width)
+	}
+	s.File.SetCellStr(s.Name, axis, header.Title)
+	if header.style > 0 {
+		s.File.SetCellStyle(s.Name, axis, axis, header.style)
+	}
+	s.Col = s.Col + 1
+}
+
+func (s *Sheet) SetValue(value interface{}) {
+	s.File.SetCellValue(s.Name, s.GetAxis(), value)
+	s.Col = s.Col + 1
+}
+
+func (s *Sheet) SetString(value string) {
+	s.File.SetCellStr(s.Name, s.GetAxis(), value)
+	s.Col = s.Col + 1
+
+}
+
+func (s *Sheet) SetInt(value int, style ...int) {
+	s.File.SetCellInt(s.Name, s.GetAxis(), value)
+	s.Col = s.Col + 1
+}
+
+func (s *Sheet) SetFloat(value float64, prec, bitSize int) {
+	s.File.SetCellFloat(s.Name, s.GetAxis(), value, prec, bitSize)
+	s.Col = s.Col + 1
+}
+
+func (s *Sheet) NextRow() {
+	s.Col = 1
+	s.Row = s.Row + 1
+}
+func (s *Sheet) NextCell() {
+	s.Col = s.Col + 1
+}
+
+type ExcelHeader struct {
+	Title string
+	Width float64
+	Key   string //key for
+	style int    //it's style ID
+}
+
+//func for how to get Value from the key defined.
+type GetCellValue func(key string, data map[string]interface{}) interface{}
+
+// simple return the value by key, if not existed, return the empty
+var GetCellValueSimple = func(key string, data map[string]interface{}) interface{} {
+	v, ok := data[key]
+	if !ok {
+		return ""
+	}
+	return v
+}
+
+var defaultStyle = &excelize.Style{
+	Alignment: &excelize.Alignment{
+		Horizontal: "center",
+		Vertical:   "center",
+	},
+	Font: &excelize.Font{
+		Bold: true,
+	},
+}
+
+type ExcelExport struct {
+	SheetName string
+	Columns   []*ExcelHeader
+	Style     *excelize.Style
+	Logger    *zap.Logger
+	Mode      GetCellValue
+}
+
+func (ee *ExcelExport) Export(f *excelize.File, data []map[string]interface{}) error {
+	if ee.Style == nil {
+		ee.Style = defaultStyle
+	}
+	if ee.Mode == nil {
+		ee.Mode = GetCellValueSimple
+	}
+
+	styleID, err := f.NewStyle(ee.Style)
+	if err != nil {
+		ee.Logger.Error("create style failed", zap.Error(err))
+		return err
+	}
+
+	if f.GetSheetIndex(ee.SheetName) == -1 {
+		f.NewSheet(ee.SheetName)
+		ee.Logger.Info("create new sheet", zap.String("sheet", ee.SheetName))
+	}
+
+	st := &Sheet{
+		File: f,
+		Name: ee.SheetName,
+		Row:  1,
+	}
+
+	index := &ExcelHeader{Title: "Index"}
+	index.style = styleID
+	st.SetHeader(index)
+	for _, item := range ee.Columns {
+		item.style = styleID
+		st.SetHeader(item)
+	}
+	ee.Logger.Info("write headers done.")
+
+	for index, row := range data {
+		st.NextRow()
+		st.SetValue(index + 1)
+		for _, item := range ee.Columns {
+			value := ee.Mode(item.Key, row)
+			// st.SetString(fmt.Sprintf("%s", value))
+			st.SetValue(value)
+			ee.Logger.Debug("set cell value", zap.Int("x", st.Row), zap.Int("y", st.Col), zap.Any("value", value))
+		}
+		ee.Logger.Debug("write row done", zap.Int("row", index))
+	}
+
+	ee.Logger.Info("write data done.", zap.String("sheet", ee.SheetName), zap.Int("rows", st.Row))
+
+	return nil
+}
---
+++ exportService.go
@@ -0,0 +1,70 @@
+package excelexport
+
+import (
+	"io"
+
+	"github.com/techquest-tech/gin-shared/pkg/query"
+	"github.com/xuri/excelize/v2"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+type ExportService struct {
+	Logger  *zap.Logger
+	DB      *gorm.DB
+	Exports []*ExportDefine
+}
+
+type ExportDefine struct {
+	Query  *query.RawQuery
+	Output *ExcelExport
+}
+
+func (es *ExportService) Init() {
+	for _, item := range es.Exports {
+		item.Output.Logger = es.Logger
+	}
+}
+
+func (es *ExportService) DoExport(params map[string]interface{}, w io.Writer) error {
+	f := excelize.NewFile()
+
+	defer f.Close()
+	anyData := false
+
+	for _, item := range es.Exports {
+		data, err := item.Query.Query(es.DB, params)
+		if err != nil {
+			es.Logger.Error("query for item failed", zap.Error(err))
+			return err
+		}
+		es.Logger.Info("read data done.", zap.Int("len", len(data)))
+
+		anyData = anyData || len(data) > 0
+
+		err = item.Output.Export(f, data)
+		if err != nil {
+			es.Logger.Error("export to sheet failed.", zap.Error(err), zap.String("sheet", item.Output.SheetName))
+			return err
+		}
+		es.Logger.Info("export to sheet done", zap.String("sheet", item.Output.SheetName))
+	}
+
+	sheet1 := f.GetSheetName(0)
+	f.DeleteSheet(sheet1)
+
+	err := f.Write(w)
+	// err := f.SaveAs(file)
+	if err != nil {
+		es.Logger.Error("flush to writer failed.", zap.Error(err))
+		return err
+	}
+
+	if !anyData {
+		es.Logger.Warn("no data for this export job.")
+	}
+
+	es.Logger.Info("export done")
+
+	return nil
+}
