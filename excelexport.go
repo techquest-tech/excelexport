086bd51fe905fb72b4847f31c58bb4d268ad30e2
jerryli <shoupifeng@163.com>
2026-07-24 11:29:36 +0800

    change export

--- excelexport.go
+++ excelexport.go
@@ -2,6 +2,8 @@ package excelexport
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/xuri/excelize/v2"
 	"go.uber.org/zap"
@@ -162,7 +164,27 @@ func (ee *ExcelExport) Export(f *excelize.File, data []map[string]interface{}) e
 			// // st.SetString(fmt.Sprintf("%s", value))
 			// st.SetValue(value)
 			// ee.Logger.Debug("set cell value", zap.Int("x", st.Row), zap.Int("y", st.Col), zap.Any("value", value))
-			values = append(values, ee.Mode(item.Key, row))
+			// values = append(values, ee.Mode(item.Key, row))
+			value := ee.Mode(item.Key, row)
+			switch v := value.(type) {
+			case string:
+				trimmed := strings.TrimSpace(v)
+				if trimmed == "" {
+					values = append(values, "")
+					continue
+				}
+				if i, errNum := strconv.ParseInt(trimmed, 10, 64); errNum == nil {
+					values = append(values, i)
+					continue
+				}
+				if f64, errNum := strconv.ParseFloat(trimmed, 64); errNum == nil {
+					values = append(values, f64)
+					continue
+				}
+				values = append(values, v)
+			default:
+				values = append(values, v)
+			}
 		}
 		cell, _ := excelize.CoordinatesToCellName(1, index+2)
 		err = streamWriter.SetRow(cell, values)
